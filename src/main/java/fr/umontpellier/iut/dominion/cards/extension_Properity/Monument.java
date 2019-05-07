827b690788406b3434143ed7e92db38ce86b6c91
yanis Elyoussoufi <yanis.el-youssoufi@etu.umontpellier.fr>
2019-05-07 16:20:44 +0200

    Extension: Prosperity
    Creation Monument

---
+++ src/main/java/fr/umontpellier/iut/dominion/cards/extension_Properity/Monument.java
@@ -0,0 +1,13 @@
+package fr.umontpellier.iut.dominion.cards.extension_Properity;
+
+import fr.umontpellier.iut.dominion.Player;
+import fr.umontpellier.iut.dominion.cards.type.Action;
+
+public class Monument extends Action {
+    public Monument() {
+        super("Monument", 4);
+    }
+    //public int getVictoryValue(Player p){
+        //return 10;
+    //}
+}
